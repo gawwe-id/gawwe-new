cd44b042529629b5cf3dfac226451aa50e93f443
Panji Gumelar <panjiggm@gmail.com>
2025-02-12 22:14:59 +0700

    create account page

---
+++ src/app/account/page.tsx
@@ -0,0 +1,9 @@
+import DasbhboardLayout from "@/layout/DashboardLayout";
+
+export default async function AccountPage() {
+  return (
+    <DasbhboardLayout>
+      <h2>ACOUNTS</h2>
+    </DasbhboardLayout>
+  );
+}
